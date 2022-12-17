cfee3b8905acd2b4287df8702b665840c7e68933
Egor Vdovenko <egor.vdovenko@natlex.ru>
2022-12-17 19:13:33 +0300

    feat: lab2_2

--- lab2/2_2/lab2_2.py
+++ lab2/2_2/lab2_2.py
@@ -1,29 +1,38 @@
 import re
 import csv
+import os
 
 text = open("text.txt", encoding="utf-8").read()
 
 # Написать программу для формирования списка названий рассказов из файла (ссылка).
 
-titlesList = re.findall("\n {5}\n[А-Я].+?\n {5}\n {5}", text)
-titlesList = list(map((lambda t: " ".join(t.split())), titlesList))
+raw_titles_list = re.findall("\n {5}\n[А-Я].+?\n {5}\n {5}", text)
+titles_list = list(map((lambda t: " ".join(t.split())), raw_titles_list))
 
 # Описать алгоритм выделения названий рассказов.
 # Для каждого рассказа сформировать отдельный файл формата txt (utf-8). Файл содержит только текст рассказа. Название файла совпадает с названием рассказа.
 
-for ti, t in enumerate(titlesList):
-    f = open('stories/{}.txt'.format(t), "w", encoding="utf-8")
-    if ti < len(titlesList) - 1:
-        f.write(text[text.find(t):text.find(titlesList[ti + 1])])
+if "stories" not in os.listdir():
+    os.mkdir("stories")
+else:
+    for file in os.listdir("stories/"):
+        os.remove("stories/" + file)
+
+for ti, t in enumerate(raw_titles_list):
+    f = open("stories/{}.txt".format(titles_list[ti]), "w", encoding="utf-8")
+    if ti < len(raw_titles_list) - 1:
+        f.write(text[text.find(t):text.find(raw_titles_list[ti + 1])])
+    else:
+        f.write(text[text.find(t):])
 
 # Отсортировать список по алфавиту.
 
-titlesList.sort()
+titles_list.sort()
 
 # Сохранить список в формате csv.
 
 titles = open("titles.csv", "w")
 write = csv.writer(titles)
 
-for t in titlesList:
+for t in titles_list:
    write.writerow(t)
